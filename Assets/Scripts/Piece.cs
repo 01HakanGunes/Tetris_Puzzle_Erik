064936a5b62eba5badcc5abd9ef117902ee6288d
HanG <01guneshakan@gmail.com>
2025-03-28 00:44:01 +0300

    falling pieces implemented

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float fallSpeed = 2f;
+    public float cellSize = 1f;
+    private Vector2Int gridPosition;
+
     void Start()
     {
-        
+        Vector3 pos = transform.position;
+        gridPosition = new Vector2Int(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            Move(-1);
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            Move(1);
     }
+
+    void Move(int dir)
+    {
+        int newX = gridPosition.x + dir;
+
+        if (newX >= 0 && newX < 4)
+        {
+            gridPosition.x = newX;
+            Vector3 newPos = new Vector3(gridPosition.x * cellSize, transform.position.y, 0);
+            transform.position = newPos;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+{
+    if (other.CompareTag("Floor"))
+    {
+        fallSpeed = 0;
+    }
+}
 }
